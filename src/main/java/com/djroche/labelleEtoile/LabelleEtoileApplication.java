4dd85f0ae1a1b50d3ee0beba74d406b69e91245b
DJRoche509 <davjroche@gmail.com>
2023-07-10 16:34:29 -0500

    First commit

---
+++ src/main/java/com/djroche/labelleEtoile/LabelleEtoileApplication.java
@@ -0,0 +1,13 @@
+package com.djroche.labelleEtoile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelleEtoileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabelleEtoileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djroche/labelleEtoile/LabelleEtoileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djroche.labelleEtoile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelleEtoileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
