7e6e88b6b0d4882e1662e4493d7a4bbc958c78b5
DJRoche509 <davjroche@gmail.com>
2023-07-12 02:18:44 -0500

    Added columns annotation

--- src/main/java/com/djroche/labelleEtoile/entities/Reservation.java
+++ src/main/java/com/djroche/labelleEtoile/entities/Reservation.java
@@ -1,4 +1,39 @@
 package com.djroche.labelleEtoile.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name= "Reservations")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @Column(name = "reservation_date")
+    private LocalDate reservationDate;
+
+    @Column(name = "date_in")
+    private LocalDate dateIn;
+
+    @Column(name = "date_out")
+    private LocalDate dateOut;
+
+    @Column(name = "data_range")
+    private Integer dataRange;
 }
