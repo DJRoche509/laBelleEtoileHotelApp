fa4d0bba0e5e6eed6bff958fdbf418935a2a7a92
DJRoche509 <davjroche@gmail.com>
2023-07-12 03:46:49 -0500

    First commit

--- src/main/java/com/djroche/labelleEtoile/entities/ReservationStatus.java
+++ src/main/java/com/djroche/labelleEtoile/entities/ReservationStatus.java
@@ -1,21 +1,4 @@
 package com.djroche.labelleEtoile.entities;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Table(name= "reservation_status")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ReservationStatus {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "status_id")
-    private Integer id;
-
-    @Column(name = "status", nullable = false)
-    private String status;
 }
