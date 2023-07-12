a18dc1a8fcd6fd8b94557005d3117ffc02160307
DJRoche509 <davjroche@gmail.com>
2023-07-12 03:36:39 -0500

    Added columns annotation

--- src/main/java/com/djroche/labelleEtoile/entities/ReservationStatus.java
+++ src/main/java/com/djroche/labelleEtoile/entities/ReservationStatus.java
@@ -1,4 +1,21 @@
 package com.djroche.labelleEtoile.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name= "reservation_status")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReservationStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "status_id")
+    private Integer id;
+
+    @Column(name = "status", nullable = false)
+    private String status;
 }
