1d2415a445dc4bf09b19ccefb2fb55dbbfc5e8d3
DJRoche509 <davjroche@gmail.com>
2023-07-12 01:59:52 -0500

    Added column annotation

--- src/main/java/com/djroche/labelleEtoile/entities/User.java
+++ src/main/java/com/djroche/labelleEtoile/entities/User.java
@@ -1,4 +1,21 @@
 package com.djroche.labelleEtoile.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private  String username;
+
+    @Column (nullable = false, unique = true)
+    private  String password;
+
+    @Column (nullable = false)
+    private boolean admin;
 }
