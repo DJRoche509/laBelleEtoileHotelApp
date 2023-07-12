8dcf37a2858e553a194f6c21b8b06d44eeb13895
DJRoche509 <davjroche@gmail.com>
2023-07-12 02:07:18 -0500

    Added columns annotation

--- src/main/java/com/djroche/labelleEtoile/entities/Room.java
+++ src/main/java/com/djroche/labelleEtoile/entities/Room.java
@@ -1,4 +1,33 @@
 package com.djroche.labelleEtoile.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name= "Rooms")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id")
+    private Long roomId;
+
+    @Column(name = "room_type")
+    private String roomType;
+
+    @Column(name = "availability")
+    private Boolean availability;
+
+    @Column(name = "ready")
+    private Boolean ready;
+
+    @Column(name = "price")
+    private Integer price;
+
+    @Column(name = "booked")
+    private Boolean booked;
 }
