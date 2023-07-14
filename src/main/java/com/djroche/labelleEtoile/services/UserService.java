8c2a28b9376e65b0390e414be276c9492073799e
DJRoche509 <davjroche@gmail.com>
2023-07-14 01:03:42 -0500

    Update with new methods implementation

--- src/main/java/com/djroche/labelleEtoile/services/UserService.java
+++ src/main/java/com/djroche/labelleEtoile/services/UserService.java
@@ -1,18 +1,56 @@
 package com.djroche.labelleEtoile.services;
 
+import com.djroche.labelleEtoile.dtos.UserDto;
 import com.djroche.labelleEtoile.entities.User;
 import com.djroche.labelleEtoile.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
+@Transactional
+// UserService class with updated methods using DTOs
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User createUser(String username, String password, boolean admin) {
+    public UserDto createUser(UserDto userDto) {
         User user = new User();
-        user
-        return userRepository.save(user);
+        user.setUsername(userDto.getUsername());
+        user.setPassword(userDto.getPassword());
+        user.setAdmin(userDto.getAdmin());
+        User savedUser = userRepository.save(user);
+        userDto.setId(savedUser.getId());
+        return userDto;
+    }
+
+    public UserDto getUserByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            return null;
+        }
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setUsername(user.getUsername());
+        userDto.setPassword(user.getPassword());
+        userDto.setAdmin(user.getAdmin());
+        return userDto;
+    }
+
+    public List<UserDto> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        List<UserDto> userDtos = new ArrayList<>();
+        for (User user : users) {
+            UserDto userDto = new UserDto();
+            userDto.setId(user.getId());
+            userDto.setUsername(user.getUsername());
+            userDto.setPassword(user.getPassword());
+            userDto.setAdmin(user.getAdmin());
+            userDtos.add(userDto);
+        }
+        return userDtos;
     }
 }
