2fa73f97a17b3f0ae24adf59ccd56105ee7b50cf
DJRoche509 <davjroche@gmail.com>
2023-07-14 00:59:13 -0500

    Added lombok annotations and new method

--- src/main/java/com/djroche/labelleEtoile/entities/User.java
+++ src/main/java/com/djroche/labelleEtoile/entities/User.java
@@ -1,10 +1,18 @@
 package com.djroche.labelleEtoile.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.djroche.labelleEtoile.dtos.UserDto;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table (name = "Users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +25,21 @@ public class User {
     @Column (name = "password", nullable = false, unique = true)
     private  String password;
 
-    @Column (name = "admin",nullable = false)
-    private boolean admin;
+    @Column (name = "admin")
+    private Boolean admin;
+
+    // Defines a one-to-one relationship between User and Customer entities, where a user can have only one customer
+    // associated with it, and a customer can have only one user associated with it.
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "user")
+    private Customer customer;
+
+    // This method returns the full name of the user if exists, else blank/null
+    public String getFullName() {
+        // Checks if the customer is not null before getting the customer's first and last name
+        if (customer != null) {
+            return customer.getFirstName() + " " + customer.getLastName();
+        } else {
+            return "";
+        }
+    }
 }
