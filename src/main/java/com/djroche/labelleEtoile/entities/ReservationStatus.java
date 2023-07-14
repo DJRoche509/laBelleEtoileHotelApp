03b0719bbfb47c175570e110fb724bfb02df866c
DJRoche509 <davjroche@gmail.com>
2023-07-14 00:49:43 -0500

    Modified ReservationStatus class + added new methods

--- src/main/java/com/djroche/labelleEtoile/entities/ReservationStatus.java
+++ src/main/java/com/djroche/labelleEtoile/entities/ReservationStatus.java
@@ -3,6 +3,7 @@ package com.djroche.labelleEtoile.entities;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -11,11 +12,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReservationStatus {
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "status_id")
-    private Integer id;
+    private Long id;
 
     @Column(name = "status")
     private String status;
+
+
+    public boolean isActive() {
+        return status.equalsIgnoreCase("active");
+    }
+
+    public boolean isCancelled() {
+        return status.equalsIgnoreCase("cancelled");
+    }
+
+    public boolean isCompleted() {
+        return status.equalsIgnoreCase("completed");
+    }
 }
