ee09b67b669bdf572753a4d72038a70cdd8689dd
DJRoche509 <davjroche@gmail.com>
2023-07-14 00:46:06 -0500

    Modified ReservationRepository class implementing findByStatus()

--- src/main/java/com/djroche/labelleEtoile/repositories/ReservationRepository.java
+++ src/main/java/com/djroche/labelleEtoile/repositories/ReservationRepository.java
@@ -1,10 +1,13 @@
 package com.djroche.labelleEtoile.repositories;
 
 import com.djroche.labelleEtoile.entities.Reservation;
+import com.djroche.labelleEtoile.entities.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReservationRepository extends JpaRepository <Reservation, Long>{
-
+    List<Reservation> findByStatus(ReservationStatus status);
 }
